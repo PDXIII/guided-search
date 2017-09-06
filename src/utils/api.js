2765d6c0662c04bc9fceb67caf32c959c4c379ba
Pete Sekan <sekan.pete@gmail.com>
2017-09-06 12:54:41 +0200

    URL changes in api and package.json

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import _ from "lodash";
 
-const url = "https://beta.json-generator.com/api/json/get/4kUm-4IdX";
-// const url = "https://raw.githubusercontent.com/PDXIII/guided-search/master/public/data/data.json";
+// const url = "https://beta.json-generator.com/api/json/get/4kUm-4IdX";
+const url = "./data/data.json";
 
 var sortData = response => ({
   items: response.data,
