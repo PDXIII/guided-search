4118580d9677ae83a999c0b02a050c3356307197
Pete Sekan <sekan.pete@gmail.com>
2017-09-06 12:48:43 +0200

    trying new deploy workflow

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import _ from "lodash";
 
-const url = "http://beta.json-generator.com/api/json/get/4kUm-4IdX";
+const url = "https://beta.json-generator.com/api/json/get/4kUm-4IdX";
 // const url = "https://raw.githubusercontent.com/PDXIII/guided-search/master/public/data/data.json";
 
 var sortData = response => ({
